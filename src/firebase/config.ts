22afb3372459ec82b75909e94bba61fbbdc06ae9
Kingsley Boateng <geniusboateng@gmail.com>
2026-01-01 19:03:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9539653970-7a846",
+  "appId": "1:900613601200:web:d93582e53c510673514875",
+  "apiKey": "AIzaSyCOZei8gqVa-qYqN3XshR0h3uukSMTWPkg",
+  "authDomain": "studio-9539653970-7a846.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "900613601200"
+};
